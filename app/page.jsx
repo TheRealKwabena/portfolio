7e40b435404a93ead2a6c9cc923396db23548df2
TheRealKwabena <gusasarkw@student.gu.se>
2024-12-01 16:55:28 -0600

    feat: implemted photo functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
+import Photo from "@/components/ui/Photo";
+
 import Social from "@/components/ui/Social";
 const Home = () => {
   return (
@@ -8,12 +10,12 @@ const Home = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Aspiring Software Engineer</span>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />
-              <span className="text-accent">Kwabena A. Boström</span>
+              <span className="text-accent">Kwabena Asare Boström</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences and I am
@@ -31,11 +33,17 @@ const Home = () => {
               </Button>
 
               <div className="mb-8 xl:mb-0">
-                <Social />
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
-          <div>photo</div>
+
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
--- components/ui/Social.jsx
+++ components/ui/Social.jsx
@@ -1,13 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import { FaGithub, FaLinkedinIn, FaTwitter} from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
 
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+];
 
-
-const Social = () => {
+const Social = ({ containerStyles, iconStyles }) => {
   return (
-    <div>Social</div>
-  )
-}
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Social
+export default Social;
